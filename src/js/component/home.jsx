8505be563406e36abc4aa1c3462a525f13576027
Ana Plata <acplata@Anas-Air.lan>
2023-07-12 20:03:53 -0400

    second

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,22 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	let counter = 0
+	setInterval(() => {
+		counter++;
+		console.log(counter);
+	}, 1500);
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container vh-100 d-flex align-items-center justify-content-center">
+			<div className="row">
+			<div className="col-2">{props.num5}</div>
+			<div className="col-2">{props.num4}</div>
+			<div className="col-2">{props.num3}</div>
+			<div className="col-2">{props.num2}</div>
+			<div className="col-2">{props.num1}</div>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,26 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(() => {
+    counter = counter + 1;
+    const firstNumber = Math.floor(counter % 10);
+    const secondNumber = Math.floor((counter / 10) % 10);
+    const thirdNumber = Math.floor((counter / 100) % 10);
+    const fourthNumber = Math.floor((counter / 1000) % 10);
+    const fifthNumber = Math.floor((counter / 10000) % 10);
+
+    console.log(
+        `Counter ${counter}`,
+        `primer numero: ${firstNumber}`,
+        `segundo numero: ${secondNumber}`,
+        `tercer numero: ${thirdNumber}`,
+        `cuarto numero: ${fourthNumber}`,
+        `quinto numero: ${fifthNumber}`,
+    );
+
+    ReactDOM.render(<Home />, document.querySelector("#app"));
+    <Home num1={firstNumber} num2={secondNumber} num3={thirdNumber} num4={fourthNumber} num5={fifthNumber} />,
+        document.querySelector("#app")
+
+}, 1000);
